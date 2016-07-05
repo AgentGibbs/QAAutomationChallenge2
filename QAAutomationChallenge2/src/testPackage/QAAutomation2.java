95acffde5a55bfa6fa5dc48980de353b12058ea5
AgentGibbs <christian.gibbs@stgconsulting.com>
2016-07-05 14:48:31 -0600

    Completed version of Code Challenge 2

--- QAAutomationChallenge2/src/testPackage/QAAutomation2.java
+++ QAAutomationChallenge2/src/testPackage/QAAutomation2.java
@@ -2,31 +2,32 @@ package testPackage;
 
 import java.util.*;
 import org.testng.*;
-import org.testng.Assert;
-import org.testng.TestException;
 import org.testng.annotations.*;
+import org.testng.asserts.*;
+import org.testng.internal.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class QAAutomation2 {
-	
+
 	public WebDriver browserDriver;
-	
+
 	private String baseUrl;
 	private List<WebElement> topNavigationMenuItems;
 	private List<WebElement> navigationSubMenuItems;
 	private String topMenuClassName;
 	private String subMenuClassName;
 	
+	
 	@BeforeTest
 	public void setUp(){
 		baseUrl= "https://www.skiutah.com";
 		topMenuClassName = "SuperfishMegaMenu-topLink";
 		subMenuClassName = "SuperfishMegaMenu-subLink";
+		Reporter reporter = new Reporter();
+		
 	}
 	
 	
@@ -34,6 +35,7 @@ public class QAAutomation2 {
 	{
 	    browserDriver.navigate().to(baseUrl);
 	    topNavigationMenuItems = browserDriver.findElements(By.className(topMenuClassName));
+	     
 	}
 	
 	public List<WebElement> getSubMenu(WebElement topMenuItem)
@@ -41,70 +43,83 @@ public class QAAutomation2 {
 		return topMenuItem.findElements(By.className(subMenuClassName));		
 	}
 	
-	public boolean verifyElement(WebElement menuItem)
+
+	private TestResult verifyElement(WebElement menuItem)
 	{
-		boolean result= false;
-		boolean aTestFailed = false;
-		String navTarget;
+		TestResult result = new TestResult();
+		result.setStatus(TestResult.STARTED);
+				
+		String navTarget = "";		
 		
-		try{
-			navTarget = menuItem.getAttribute("href");
-			System.out.println(navTarget);
+		String logMessage = "Verifying Menu Item: " + menuItem.getText() + System.lineSeparator();
+		
+		navTarget = getHref(menuItem);
+		
+		if(navTarget == "")
+		{	
+			String errMessage = "No href link found for menu item " + menuItem.getText() + ": Skipping Test.";
+			logMessage  = logMessage + errMessage + System.lineSeparator();
+			result.setStatus(TestResult.SKIP);
 		}
 		
-		catch(Error E){
-			String errmsg = "No href link exists for menu item " + menuItem.getAttribute("title");
-			result = false;
-			throw new TestException(errmsg, new java.lang.Throwable());
+		else
+		{
+			logMessage = logMessage+ "Expected web page: "+ navTarget + System.lineSeparator();
+			menuItem.click();
+			String cUrl = browserDriver.getCurrentUrl();
+			logMessage = logMessage + "Actual web page: "+ cUrl+ System.lineSeparator();
+			if(cUrl==navTarget){
+				result.setStatus(TestResult.SUCCESS);
+				logMessage = logMessage + "SUCCESS";
+			}
+			else {result.setStatus(TestResult.FAILURE);}
+			logMessage = logMessage + "FAIL";
 		}
 		
-		menuItem.click();
-		String cUrl = browserDriver.getCurrentUrl();
-		System.out.println(cUrl);
-		System.out.println(navTarget);
-		if(cUrl.contains(navTarget)){
-			
-			result=true;
-		}
-		
+		Reporter.log(logMessage);
+		System.out.println(logMessage);
 		return result;
 	}	
+
+	
+	
+	private String getHref(WebElement link)
+	{
+		String returnValue = "";
+		if(link.getAttribute("href")!= null)
+		{
+			returnValue = link.getAttribute("href");
+		}
+		
+		
+		return returnValue;
+	}
 	
 	
 	@Test
 	public void verifyChrome()
-	{
+	{		
+		boolean allTestPass = true;
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Christian Gibbs\\Java Libraries\\selenium-2.53.0\\chromedriver.exe");
 	    browserDriver = new ChromeDriver();
 	    getMainMenu();
+	    Reporter.log("Launching ChromDriver tests");
+	    
 	    
 	    for(int i = 0; i<topNavigationMenuItems.size(); i++)
 	    {
-	    	//if(browserDriver.getCurrentUrl() != baseUrl)
-	    	{
-	    		System.out.println("Actual URL: " + browserDriver.getCurrentUrl());
-	    	}//end if
-	    		    	
-	    	System.out.println("Before click: " + browserDriver.getCurrentUrl());
-	    	WebElement menuItemToTest = topNavigationMenuItems.get(5);
-	    	Boolean navigatedSuccessfully = verifyElement(menuItemToTest);
-	    	System.out.println(navigatedSuccessfully.booleanValue());
-	    	//Assert.assertEquals(navigatedSuccessfully, true );
-	    	System.out.println("After click: " + browserDriver.getCurrentUrl());
-	    	/*navigationSubMenuItems = getSubMenu(menuItemToTest);
-	    	for(int nestedI = 0; nestedI < navigationSubMenuItems.size(); nestedI ++)
-	    	{
-	    		if(browserDriver.getCurrentUrl()!= baseUrl)
-	    		{
-	    			browserDriver.get(baseUrl);	    	
-	    		}//end if
+	    	WebElement menuItemToTest = topNavigationMenuItems.get(i);
+	    	TestResult navigatedSuccessfully = verifyElement(menuItemToTest);
+	    	if (navigatedSuccessfully.getStatus() == TestResult.FAILURE)
+	    			{
+	    			allTestPass = false;
+	    			}
 	    	
-	    		boolean navigatedSubMenu = verifyElement(navigationSubMenuItems.get(nestedI));
-	    		Assert.assertEquals(navigatedSubMenu, true ); 
-		    }//end nested for loop
-	    	*/
+	    	getMainMenu();
 	    }
-//	    browserDriver.close();
+	    assert(allTestPass == true);
+	    
+	    browserDriver.close();
 	   
 	}
 
